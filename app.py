75b65174b19dd65178568fe4cb80c73ee6050ef4
amitkrishna <amitkrishna936@gmail.com>
2021-12-02 21:47:01 +0530

    fix: added serv val

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, make_response
 import db
+import logger
 
 app = Flask(__name__)
 
@@ -8,13 +9,18 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         db.add_comment(request.form['comment'])
+        logger.info(request.form['comment'])
 
     search_query = request.args.get('q')
+    logger.info(search_query)
 
     comments = db.get_comments(search_query)
+    logger.info(comment)
     response = make_response(
         render_template('index.html', comments=comments, search_query=search_query)
         )
+    logger.info(response)
     response.set_cookie("cookieInfo", "some sensitive data")
+    logger.info()
     # response.headers.set('Content-Security-Policy', "script-src 'none'")
     return response
